2d6a04f276b6d70c0e810a7aeabca9a96a34c473
Anirudh Negi <148575136+anirudhnegi03@users.noreply.github.com>
2025-10-15 21:51:14 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const WEBSOCKET_URL = 'wss://your-backend-name.onrender.com';
+const WEBSOCKET_URL = 'wss://transaction-anomaly-backend.onrender.com';
 
 function App() {
   const [transactions, setTransactions] = useState([]);
